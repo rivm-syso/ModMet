fe175cf9e5aa24da491b02c94bd39b5c88cad595
Marte Voorneveld <42668910+martedv@users.noreply.github.com>
2026-08-11 17:22:56 +0200

    Feature/add ci versioning (#13)
    * add version tagging to the github ci
    
    * added code coverage as citizen of ci, it is now a step in creating a release and should update the tag
    
    * small linter fix
    
    * removed deprecated function
    
    * update actions to latest version

---
+++ src/modmet_version.f90
@@ -0,0 +1,18 @@
+!------------------------------------------------------------------------------
+! Module:     modmet_version
+! Authors:    Marte Voorneveld, RIVM
+! Created:    August 11 2026
+! Updated:    August 11 2026
+! Description:
+!   Version metadata for the ModMet public API.
+!   This module is updated by CI when creating automated releases.
+!------------------------------------------------------------------------------
+module modmet_version
+   implicit none (type, external)
+   private
+
+   character(len=*), parameter, public :: VERSION = "1.2.0"
+   character(len=*), parameter, public :: RELEASE_DATE = "2026-08-11"
+   character(len=*), parameter, public :: BUILD_DATE = RELEASE_DATE
+
+end module modmet_version
